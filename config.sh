081b64a2e52c786cba432b510e75187c26f6cdbf
AbigailXu <qbxzz233@gmail.com>
2021-05-23 05:11:15 -0400

    upload files

---
+++ config.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+  
+
+course_code="cs246"
+
+assignment_file_path="../"
+
+assignment_number="1"
+submit_assignment="t"
+
+question_number="1"
+submit_question="f"
+
+bash submit.sh  ${course_code} ${assignment_file_path} ${assignment_number} ${submit_assignment} ${question_number} ${submit_question}
---
+++ submit.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+course=${1}
+filePath=${2}
+a=${3}
+sub_a=${4}
+q=${5}
+sub_q=${6}
+
+source /u/cs246/setup
+
+if [[ $sub_a == "t" ]] ; then
+        ls ${filePath} | egrep "a${a}q[0-100][a-z]?.txt" > filesList.sh
+        while read line; do
+                echo "- Submit $line"
+                st=1
+                ed=5
+                aTitle=$(echo ${line} | cut -c${st}-${ed})
+                marmoset_submit ${course} ${aTitle} ${filePath}${line}
+        done < filesList.sh
+elif [[ $sub_q == "t" ]] ; then
+        ls ${filePath} | egrep "a${a}q${q}[a-z]?.txt" > filesList.sh
+        while read line; do
+                echo "- Submit $line"
+                st=1
+                ed=5
+                aTitle=$(echo ${line} | cut -c${st}-${ed})
+                marmoset_submit ${course} ${aTitle} ${filePath}${line}
+        done < filesList.sh
+else
+        echo "Error, please check config.sh."
+fi
+
+echo "Completed."
+
+exit 0
